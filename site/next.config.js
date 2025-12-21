e415510427589c6f06698bc0e0e46e1e34adf7a4
James Konderla <james.konderla@konderla.org>
2025-12-21 15:10:14 -0600

    Fix storefront basePath and Tailwind content scanning

--- site/next.config.js
+++ site/next.config.js
@@ -1,21 +1,24 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
-  output: 'export',
-  distDir: '../public',
+  output: "export",
+  distDir: "../public",
   env: {
-    name: 'SquirrelWorks Kasm Registry',
-    description: 'SquirrelWorks Kasm Workspace Registry.',
-    icon: 'https://squirrelworksllc.github.io/kasm-registry/1.1/swlogo.png',
-    listUrl: 'https://squirrelworksllc.github.io/kasm-registry/',
-    contactUrl: '',
+    name: "SquirrelWorks Kasm Registry",
+    description: "SquirrelWorks Kasm Workspace Registry.",
+    icon: "https://squirrelworksllc.github.io/kasm-registry/1.1/swlogo.png",
+    listUrl: "https://squirrelworksllc.github.io/kasm-registry/",
+    contactUrl: "",
   },
   reactStrictMode: true,
-  basePath: '/kasm-registry/1.0',
+
+  // IMPORTANT: storefront lives under /kasm-registry/1.1 on GitHub Pages
+  basePath: "/kasm-registry/1.1",
+
   trailingSlash: true,
   images: {
     unoptimized: true,
-  }
-}
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
